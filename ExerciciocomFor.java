26a17f8335fe23d97e88c21cd5f16fb7ee696c61
Pedro Henrique Moreira <80130337+PedroBola8@users.noreply.github.com>
2024-11-27 10:41:03 -0300

    Add files via upload

---
+++ ExerciciocomFor.java
@@ -0,0 +1,23 @@
+package Casa;
+
+public class ExerciciocomFor {
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println("olá!");
+			}
+		
+		
+		for (int i = 1; i < 100; i++) {
+			if (i % 3 == 0) {
+			System.out.println(i);
+			break;
+			}
+			}
+		
+		for (int N = 10;N>=1;N--) {
+			System.out.println(N);
+		}
+	}
+
+}
